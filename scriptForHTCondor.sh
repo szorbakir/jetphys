87d1f3db79fafe6c91693c37c5740b9f2b1c2768
ibrahim <ibrahim.soner.zorbakir@cern.ch>
2019-07-11 14:58:05 +0200

    data files, ignore file, and HTCondor scripts

---
+++ scriptForHTCondor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /afs/cern.ch/work/i/izorbaki/dijet_analysis/DATA2017/ERAs/RunC/jetphys
+
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.06.04/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+
+start_time="$(date -u +%s)"
+root -l -b -q mk_HistosFill.C
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+
+echo "Total of $elapsed seconds elapsed for process"
