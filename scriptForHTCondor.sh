42d5ec09cb62e9f16f3861138fa06703231be69d
ibrahim <ibrahim.soner.zorbakir@cern.ch>
2019-10-09 12:28:55 +0200

    deleting a file

--- scriptForHTCondor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /afs/cern.ch/work/i/izorbaki/dijet_analysis/MC2017/jetphys
-
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.06.04/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
-
-root -l -b -q mk_HistosFill.C
